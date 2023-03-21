37e21885420f5659bd12a2242f38e9cfd7f536c4
AngelaZabriesak <84974867+AngelaZabriesak@users.noreply.github.com>
2023-03-21 15:13:28 +0100

    Update Bag.java

--- src/main/java/it/polimi/ingsw/Bag.java
+++ src/main/java/it/polimi/ingsw/Bag.java
@@ -1,4 +1,23 @@
 package it.polimi.ingsw;
 
+import java.util.ArrayList;
+
 public class Bag {
+
+    // attributi
+    private static final int SIZE = 6;
+    private int[] numItem = new int[SIZE];
+    //metodi
+
+    public Bag() {
+
+    }
+
+    public int[] getNumItem() {
+        return numItem;
+    }
+
+    public void setNumItem(int[] numItem) {
+        this.numItem = numItem;
+    }
 }
