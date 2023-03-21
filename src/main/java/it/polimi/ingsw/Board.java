b381dcee3bfa19559ed0180e3d97bbac175b10b4
Mattia Asinari <mattia.asinari@mail.polimi.it>
2023-03-21 15:22:17 +0100

    aggiornato classe board

--- src/main/java/it/polimi/ingsw/Board.java
+++ src/main/java/it/polimi/ingsw/Board.java
@@ -1,4 +1,21 @@
 package it.polimi.ingsw;
 
 public class Board {
+
+    //attributi
+
+    private static final int ROW = 9;
+    private static final int COL = 9;
+    int myBoard[][] = new int[ROW][COL];
+
+    //metodi
+
+
+    public Board() {
+
+    }
+
+    public void setPositionAvailable() {
+
+    }
 }
