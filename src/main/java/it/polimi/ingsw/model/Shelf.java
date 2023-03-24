cb245f26c695d4d7ab1b650716e9bc6c2d3ff88c
AngelaZabriesak <84974867+AngelaZabriesak@users.noreply.github.com>
2023-03-24 14:43:24 +0100

    shelf

--- src/main/java/it/polimi/ingsw/model/Shelf.java
+++ src/main/java/it/polimi/ingsw/model/Shelf.java
@@ -1,18 +1,36 @@
 package it.polimi.ingsw.model;
 
+import it.polimi.ingsw.model.Bag.Item;
+
 public class Shelf {
     private static final int ROW = 6;
     private static final int COL = 5;
-    int myShelf[][] = new int[ROW][COL];
+    Item myShelf[][] = new Item[ROW][COL];
 
     public Shelf() {
 
     }
 
-    public void setItemInShelf(int col){
+    public void setItemInShelf(int col,Item item){
         for(int r = 0;r<ROW;r++){
-
+            if(myShelf[r][col]==null){
+                myShelf[r][col] = item;
+            }
         }
+    }
+
+    /**
+     * @return the number of cells empty
+     */
+    public int getNEmpty(){
+        int nEmpty = ROW*COL;
+        for(int r=0;r<ROW;r++) {
+            for (int c = 0; c < COL; c++) {
+                if(myShelf[r][c]!=null)
+                    nEmpty--;
+            }
+        }
+        return nEmpty;
     }
 
 }
