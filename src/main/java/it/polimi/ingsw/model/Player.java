937003b08d3f31cde77026a912b959e8c5f9484d
Elena Tortorella <elena.tortorella@mail.polimi.it>
2023-03-24 14:58:17 +0100

    Update Player.java

--- src/main/java/it/polimi/ingsw/model/Player.java
+++ src/main/java/it/polimi/ingsw/model/Player.java
@@ -4,23 +4,41 @@ import it.polimi.ingsw.model.Bag.Item;
 
 public class Player {
 
-    // attributi  (manca goal)
 
     private Shelf myShelf;
     private String nickname;
+    private Goal myGoal;
     private int myScore;
     private Item myItem;
 
-    //metodi
+
+
+    // return the player nickname
+    public String getNickname() {
+        return this.nickname;
+    }
+
+    //return the player Shelf
 
     public Shelf getMyShelf() {
         return myShelf;
     }
 
-    public String getNickname() {
-        return nickname;
+    // return the player pGoal
 
+    public Goal getMyGoal() {
+        return myGoal;
+    }
 
+    // return the player score
+
+    public int getMyScore() {
+        return myScore;
+    }
+
+    // return the item taken from the bag during player's turn
+    public Item getMyItem() {
+        return myItem;
     }
 }
 
