9ae9885d4f38354a65c887083250eff7c21fe923
Elena Tortorella <elena.tortorella@mail.polimi.it>
2023-04-03 18:06:36 +0200

    update common goal 6, and common goal 1

--- src/main/java/it/polimi/ingsw/model/Goal/CommonGoal/CommonGoal6.java
+++ src/main/java/it/polimi/ingsw/model/Goal/CommonGoal/CommonGoal6.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.model.Goal.CommonGoal;
 
 import it.polimi.ingsw.model.Bag.Item;
+import it.polimi.ingsw.model.Shelf;
 import it.polimi.ingsw.model.Token;
 
 //common goal 6:
@@ -8,22 +9,22 @@ import it.polimi.ingsw.model.Token;
 
 public class CommonGoal6 extends Cgoal{
 
-    private static final int ROW = 6;
-    private static final int COL = 5;
-    private Item[][] myItemGoal = new Item[ROW][COL];
-    private int numOfCond = 0;                 // for this goal >0 at the end
 
-    public CommonGoal6(){
+    public boolean CommonGoal6(Shelf CShelf) {
         setDescription("common goal 6:five tiles of the same type forming a diagonal\n");
         setIndex(6);
-        myItemGoal[0][0] = myItemGoal[1][1]=myItemGoal[2][2]=myItemGoal[3][3]=myItemGoal[4][4];
-        myItemGoal[0][4] = myItemGoal[1][3]=myItemGoal[2][2]=myItemGoal[3][1]=myItemGoal[4][0];
-        myItemGoal[5][0] = myItemGoal[4][1]=myItemGoal[3][2]=myItemGoal[2][3]=myItemGoal[1][4];
-        myItemGoal[5][4] = myItemGoal[4][3]=myItemGoal[3][2]=myItemGoal[2][1]=myItemGoal[1][0];
 
-
+        Item[][] myshelf = CShelf.getMyShelf();
+        if ((myshelf[0][0].getColor() == myshelf[1][1].getColor() && myshelf[0][0].getColor() == myshelf[2][2].getColor() && myshelf[0][0].getColor() == myshelf[3][3].getColor() && myshelf[0][0].getColor() == myshelf[4][4].getColor()) ||
+                (myshelf[0][4].getColor() == myshelf[1][3].getColor() && myshelf[0][4].getColor() == myshelf[2][2].getColor() && myshelf[0][4].getColor() == myshelf[3][1].getColor() && myshelf[0][4].getColor() == myshelf[4][0].getColor()) ||
+                (myshelf[5][0].getColor() == myshelf[4][1].getColor() && myshelf[5][0].getColor() == myshelf[3][2].getColor() && myshelf[5][0].getColor() == myshelf[2][3].getColor() && myshelf[5][0].getColor() == myshelf[1][4].getColor()) ||
+                (myshelf[5][4].getColor() == myshelf[4][3].getColor() && myshelf[5][4].getColor() == myshelf[3][2].getColor() && myshelf[5][4].getColor() == myshelf[2][1].getColor() && myshelf[5][4].getColor() == myshelf[1][0].getColor())) {
+            return true;
+        } else {
+            return false;
+        }
 
     }
 
+    }
 
-}
