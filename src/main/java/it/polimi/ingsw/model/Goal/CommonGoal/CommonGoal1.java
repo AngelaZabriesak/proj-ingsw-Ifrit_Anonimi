eefc81b9e8195c726358d0f7aee6bde735bef53a
Elena Tortorella <elena.tortorella@mail.polimi.it>
2023-04-03 16:59:12 +0200

    Update CommonGoal1.java

--- src/main/java/it/polimi/ingsw/model/Goal/CommonGoal/CommonGoal1.java
+++ src/main/java/it/polimi/ingsw/model/Goal/CommonGoal/CommonGoal1.java
@@ -3,28 +3,21 @@ package it.polimi.ingsw.model.Goal.CommonGoal;
 import it.polimi.ingsw.model.Bag.ColorItem;
 import it.polimi.ingsw.model.Bag.Item;
 import it.polimi.ingsw.model.Token;
+import it.polimi.ingsw.model.Shelf;
 
 // common goal 1:
 // four tiles of the same type in the four corners of the bookshelf
 
 public class CommonGoal1 extends Cgoal {
 
+    public boolean CommonGoal1(Shelf CShelf) {
 
+        Item[][] myshelf = CShelf.getMyShelf();
+        if (myshelf[0][0].getColor() == myshelf[0][4].getColor() && myshelf[0][0].getColor() == myshelf[5][4].getColor() && myshelf[0][0].getColor() == myshelf[5][0].getColor()) {
 
-    public CommonGoal1() {
-        setDescription("common goal 1:four tiles of the same type in the four corners of the bookshelf\n");
-        setIndex(1);
-
-    }
-
-      if(myItemGoal[0][0] = Item[0][4] = Item[5][0] = Item[5][4]){
-        numOfCond++;
-    }
-
-    public boolean CheckCondition(){
-
-        if(numOfCond>0){
             return true;
+        } else {
+            return false;
         }
-        return false;
     }
+}
