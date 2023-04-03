f6f63eb15993d8fc1577bb12c8ff55190fb242b7
Mattia Asinari <mattia.asinari@mail.polimi.it>
2023-04-03 18:03:58 +0200

    update commongoal8 (da migliorare)

--- src/main/java/it/polimi/ingsw/model/Goal/CommonGoal/CommonGoal8.java
+++ src/main/java/it/polimi/ingsw/model/Goal/CommonGoal/CommonGoal8.java
@@ -2,6 +2,8 @@ package it.polimi.ingsw.model.Goal.CommonGoal;
 
 import it.polimi.ingsw.model.Bag.Item;
 import it.polimi.ingsw.model.Token;
+import it.polimi.ingsw.model.Shelf;
+
 
 // common goal 8:
 // two columns each formed by 6 different types of tiles
@@ -11,11 +13,26 @@ public class CommonGoal8 extends Cgoal{
     private static final int ROW = 6;
     private static final int COL = 5;
     private Item[][] myItemGoal = new Item[ROW][COL];
-    private int numOfCond = 0;                 // for this goal >1 at the end
+                     // for this goal >1 at the end
 
-    public CommonGoal8(){
+    public CommonGoal8(Shelf CShelf){
         setDescription("common goal 8:two columns each formed by 6 different types of tiles\n");
         setIndex(8);
+        Item[][] myshelf = CShelf.getMyShelf();
+        int numOfCond = 0;
+        int i=0;
+        int j=0;
+        do {
+            if(myshelf[i][j] == myshelf[i][j+1]) {
+                i++;
+            } else {
+                if(j==ROW)
+                    numOfCond++;
+                    j++;
+                }
+        } while (i<COL);
+
+
     }
 
 }
