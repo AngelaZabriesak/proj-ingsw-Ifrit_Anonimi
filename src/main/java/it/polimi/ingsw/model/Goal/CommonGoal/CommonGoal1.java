7ff7055faefff6b7fd48bd38af426cfd3cf7a20c
Elena Tortorella <elena.tortorella@mail.polimi.it>
2023-04-03 15:50:24 +0200

    update common goal

--- src/main/java/it/polimi/ingsw/model/Goal/CommonGoal/CommonGoal1.java
+++ src/main/java/it/polimi/ingsw/model/Goal/CommonGoal/CommonGoal1.java
@@ -9,15 +9,22 @@ import it.polimi.ingsw.model.Token;
 
 public class CommonGoal1 extends Cgoal {
 
-    private static final int ROW = 6;
-    private static final int COL = 5;
-    private Item[][] myItemGoal = new Item[ROW][COL];
-    private final int numOfCond = 0;                 // for this goal >0 at the end
+
 
     public CommonGoal1() {
         setDescription("common goal 1:four tiles of the same type in the four corners of the bookshelf\n");
         setIndex(1);
-        myItemGoal[0][0] = myItemGoal[0][4] = myItemGoal[5][0] = myItemGoal[5][4];
+
     }
 
-}
+      if(myItemGoal[0][0] = Item[0][4] = Item[5][0] = Item[5][4]){
+        numOfCond++;
+    }
+
+    public boolean CheckCondition(){
+
+        if(numOfCond>0){
+            return true;
+        }
+        return false;
+    }
--- src/main/java/it/polimi/ingsw/model/Goal/CommonGoal/CommonGoal10.java
+++ src/main/java/it/polimi/ingsw/model/Goal/CommonGoal/CommonGoal10.java
@@ -13,9 +13,21 @@ public class CommonGoal10 extends Cgoal{
     private Item[][] myItemGoal = new Item[ROW][COL];
     private int numOfCond = 0;                 // for this goal >0 at the end
 
-    public CommonGoal10(){
+    public CommonGoal10() {
         setDescription("common goal 10:five tiles of the same type forming an X\n");
         setIndex(10);
+        int i = ; // i che puo' essere (0,1,2)
+        int j = i + 2;
+        int u = i + 1;
+        int h;     // h che puo essere da (0,1,2,3)
+        int k = h + 2;
+        int q = h + 1;
+
+        if ((i = (0 | 1 | 2)) & (h = (0 | 1 | 2 | 3))) {
+
+            myItemGoal[h][i] = myItemGoal[k][i] = myItemGoal[h][j] = myItemGoal[k][j] = myItemGoal[q][u];
+
+        }
     }
 
 }
