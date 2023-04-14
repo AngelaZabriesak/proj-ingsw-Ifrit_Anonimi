a75f90535b281b3c59c6c3d807eca86fe45b3059
Mattia Asinari <mattia.asinari@mail.polimi.it>
2023-04-14 15:10:38 +0200

    Update CommonGoal0.java

--- src/main/java/it/polimi/ingsw/model/Goal/CommonGoal/CommonGoal0.java
+++ src/main/java/it/polimi/ingsw/model/Goal/CommonGoal/CommonGoal0.java
@@ -1,7 +1,11 @@
 package it.polimi.ingsw.model.Goal.CommonGoal;
 
 import it.polimi.ingsw.model.Bag.Item;
+import it.polimi.ingsw.model.Game.CreateItemGroup;
+import it.polimi.ingsw.model.Position;
 import it.polimi.ingsw.model.Shelf;
+
+import java.util.ArrayList;
 
 /* common goal 0 :
                     six groups each containing at least 2 tiles of the same type,
@@ -20,32 +24,19 @@ public class CommonGoal0 extends Cgoal {
 
     @Override
     public boolean isTaken(Shelf myShelf) {
-        Item[][] myshelf = myShelf.getMyShelf();
-        Item[][] myshelfcpy = myShelf.getMyShelf();
-        int numOfCond = 0;     // for this goal ==6
 
-        for (int i=0; i<ROW; i++){
-            for (int j=0; j<COL; j++){
-                if
-                (myshelf[i][j].getColor() != null &&
-                        myshelf[i][j].getColor() == myshelf[i+1][j].getColor() &&
-                        myshelf[i][j].getColor() == myshelfcpy[i][j].getColor())
-                {numOfCond++;
-                    myshelfcpy[i][j] = null;
-                    myshelfcpy[i+1][j] = null;}
-                if
-                (myshelf[i][j].getColor() != null &&
-                        myshelf[i][j].getColor() == myshelf[i][j+1].getColor() &&
-                        myshelf[i][j].getColor() == myshelfcpy[i][j].getColor() )
-                {numOfCond++;
-                    myshelfcpy[i][j] = null;
-                    myshelfcpy[i][j+1] = null;}
+        ArrayList<Position>[] groups = new CreateItemGroup().creaGruppi(getGame().getCurrentPlayer());
+        int numOfCond=0;
 
-                return numOfCond ==6;
+        for (int i=0; i<= groups.length; i++)
+        {
+            ArrayList<Position> group = groups[i];
+            numOfCond += group.size()/2;
+            if(numOfCond >= 6) {
+                break;
             }
         }
-        return false;
+        return numOfCond >= 6;
     }
 }
-
 
