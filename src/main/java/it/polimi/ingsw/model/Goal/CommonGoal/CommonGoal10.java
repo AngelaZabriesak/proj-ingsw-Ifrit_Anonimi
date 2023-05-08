ba244611f46da946ff51efffaa4687ed423fb3c7
Elena Tortorella <elena.tortorella@mail.polimi.it>
2023-05-08 16:46:58 +0200

    aggiustato common goal 10

--- src/main/java/it/polimi/ingsw/model/Goal/CommonGoal/CommonGoal10.java
+++ src/main/java/it/polimi/ingsw/model/Goal/CommonGoal/CommonGoal10.java
@@ -26,10 +26,10 @@ public class CommonGoal10 extends Cgoal{
         for (int i=0; i<=3; i++) {
             for (int j = 0; j<=4; j++) {
                 if (myshelf[i][j] != null &&
-                        myshelf[j][i].getColor() == myshelf[j][i + 2].getColor() &&
-                                myshelf[j][i].getColor() == myshelf[j + 1][i + 1].getColor() &&
-                                myshelf[j][i].getColor() == myshelf[j + 2][i].getColor() &&
-                                myshelf[j][i].getColor() == myshelf[j + 2][i + 2].getColor())
+                        (myshelf[j][i + 2] != null && myshelf[j][i].getColor() == myshelf[j][i + 2].getColor() ) &&
+                        (myshelf[j + 1][i + 1] != null && myshelf[j][i].getColor() == myshelf[j + 1][i + 1].getColor() ) &&
+                        (myshelf[j + 2][i] != null && myshelf[j][i].getColor() == myshelf[j + 2][i].getColor() ) &&
+                        ( myshelf[j + 2][i + 2] != null && myshelf[j][i].getColor() == myshelf[j + 2][i + 2].getColor() ) )
                     return true;
 
             }
