53006c7089838885bc9ae4c2a1b19d164f2ea01e
Mattia Asinari <mattia.asinari@mail.polimi.it>
2023-05-08 15:49:00 +0200

    commongoal3 fixes

--- src/main/java/it/polimi/ingsw/Model/Goal/CommonGoal/CommonGoal10.java
+++ src/main/java/it/polimi/ingsw/Model/Goal/CommonGoal/CommonGoal10.java
@@ -10,6 +10,8 @@ import it.polimi.ingsw.Model.Token;
 
 public class CommonGoal10 extends Cgoal{
 
+    private static final int ROW = 6;
+    private static final int COL = 5;
 
     public CommonGoal10() {
         setDescription("common goal 10:five tiles of the same type forming an X\n");
