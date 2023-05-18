d68240079e8bc266f87122838117e0c4db92f642
Mattia Asinari <mattia.asinari@mail.polimi.it>
2023-05-18 16:41:48 +0200

    gui first tries, menuscene loginscene

---
+++ src/main/java/it/polimi/ingsw/View/Scene/LoginScene.java
@@ -0,0 +1,23 @@
+package it.polimi.ingsw.View.Scene;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginScene {
+
+    @FXML
+    private Button loginbutton;
+
+    @FXML
+    private AnchorPane pane;
+
+    @FXML
+    private TextField textfield;
+
+    @FXML
+    private Label textlabel;
+
+}
---
+++ src/main/java/it/polimi/ingsw/View/Scene/MenuScene.java
@@ -0,0 +1,19 @@
+package it.polimi.ingsw.View.Scene;
+
+import it.polimi.ingsw.Observer.ViewObservable;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+public class MenuScene {
+
+    @FXML
+    private Button exitbutton;
+
+    @FXML
+    private AnchorPane pane;
+
+    @FXML
+    private Button playbutton;
+
+}
