8477f69fc0fe06696aac8d1479d43a69b1e9502d
Mattia Asinari <mattia.asinari@mail.polimi.it>
2023-05-22 11:29:50 +0200

    update

---
+++ src/main/java/it/polimi/ingsw/View/Scene/GameScene.java
@@ -0,0 +1,75 @@
+package it.polimi.ingsw.View.Scene;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+
+public class GameScene {
+
+    @FXML
+    private ImageView arrowcol0;
+
+    @FXML
+    private ImageView arrowcol1;
+
+    @FXML
+    private ImageView arrowcol2;
+
+    @FXML
+    private ImageView arrowcol3;
+
+    @FXML
+    private ImageView arrowcol4;
+
+    @FXML
+    private GridPane boardgrid;
+
+    @FXML
+    private ImageView boardimg;
+
+    @FXML
+    private TextField chatinserttext;
+
+    @FXML
+    private Label chatlabel;
+
+    @FXML
+    private HBox commongoalboxdx;
+
+    @FXML
+    private HBox commongoalboxsx;
+
+    @FXML
+    private GridPane itemordergrid;
+
+    @FXML
+    private ImageView itemorderimg;
+
+    @FXML
+    private GridPane myshelfgrid;
+
+    @FXML
+    private ImageView myshelfimg;
+
+    @FXML
+    private AnchorPane pane;
+
+    @FXML
+    private Label personalgoallabel;
+
+    @FXML
+    private VBox personalgolbox;
+
+    @FXML
+    private StackPane rootPane;
+
+    @FXML
+    private ImageView uselessarrow;
+
+}
