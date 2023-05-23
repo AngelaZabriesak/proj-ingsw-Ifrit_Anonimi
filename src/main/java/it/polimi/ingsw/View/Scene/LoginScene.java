3ee0c68a7ea23a4b456bd15e8b0f4f7513a8e955
Elena Tortorella <elena.tortorella@mail.polimi.it>
2023-05-23 16:10:35 +0200

    login scene

--- src/main/java/it/polimi/ingsw/View/Scene/LoginScene.java
+++ src/main/java/it/polimi/ingsw/View/Scene/LoginScene.java
@@ -41,6 +41,14 @@ public class LoginScene extends InputObservable implements GenericScene {
     }
 
     private void onPlayButtonClick(Event event) {
+        String defaultAddress = "127.0.0.1";
+        if(nicknameField!=null){
+            defaultAddress = nicknameField.getText();
+        }
+        loginButton.setDisable(true);
+
+        String finalDefaultAddress = defaultAddress;
+        new Thread(() -> notifyInObserver(obs -> obs.onUpdateServerInfo(finalDefaultAddress, 16847))).start();
         ChangeScene.changeRootPane(observers, event, "select_np_scene.fxml");
     }
 
