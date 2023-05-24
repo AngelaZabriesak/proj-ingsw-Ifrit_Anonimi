4d33644665b3dd8213e1ed315d5ce9ec16f0b694
Mattia Asinari <mattia.asinari@mail.polimi.it>
2023-05-24 17:58:52 +0200

    shelf comparing scenes

---
+++ src/main/java/it/polimi/ingsw/View/Scene/FourPCompareScene.java
@@ -0,0 +1,58 @@
+package it.polimi.ingsw.View.Scene;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+
+public class FourPCompareScene {
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private AnchorPane pane;
+
+    @FXML
+    private GridPane playerFourGridPane;
+
+    @FXML
+    private ImageView playerFourShelf;
+
+    @FXML
+    private HBox playerFourUsernameBox;
+
+    @FXML
+    private GridPane playerThreeGridPane;
+
+    @FXML
+    private ImageView playerThreeShelf;
+
+    @FXML
+    private HBox playerThreeUsernameBox;
+
+    @FXML
+    private GridPane playerTwoGridPane;
+
+    @FXML
+    private ImageView playerTwoShelf;
+
+    @FXML
+    private HBox playerTwoUsernameBox;
+
+    @FXML
+    private StackPane rootPane;
+
+    @FXML
+    private GridPane selfGridPane;
+
+    @FXML
+    private ImageView selfShelf;
+
+    @FXML
+    private HBox selfUsernameBox;
+
+}
