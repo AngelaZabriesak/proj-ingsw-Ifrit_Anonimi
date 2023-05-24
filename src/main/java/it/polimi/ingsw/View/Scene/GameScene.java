2de8e573d080ceaac2740707ed0ab37369a4464b
Elena Tortorella <elena.tortorella@mail.polimi.it>
2023-05-24 12:17:22 +0200

    prove per la matrice

--- src/main/java/it/polimi/ingsw/View/Scene/GameScene.java
+++ src/main/java/it/polimi/ingsw/View/Scene/GameScene.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.View.Scene;
 
+import it.polimi.ingsw.Model.Game.Game;
 import it.polimi.ingsw.Observer.InputObservable;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -10,6 +11,8 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
 public class GameScene extends InputObservable implements GenericScene {
 
@@ -73,4 +76,63 @@ public class GameScene extends InputObservable implements GenericScene {
     @FXML
     private ImageView uselessArrow;
 
+
 }
+
+
+
+
+
+
+
+        /*   funzione che implementa la matrice gui
+
+    public class BoardGui extends InputObservable{
+        private int[][] boardGui;
+
+        public BoardGui(int[][] boardGui){
+            this.boardGui = boardGui;
+        }
+
+
+
+        public void start(Stage primaryStage){
+
+            GridPane gridPane = new GridPane();
+
+            for (int i=0; i<boardGui.length; i++){
+                for (int j=0; j<boardGui[i].length; j++)  {
+                    int value = boardGui[i][j];
+
+                    Label label= new Label(Integer.toString(value));
+                gridPane.add(label,j,i);
+
+                }
+            }
+
+            altra prova
+
+
+            public class GuiBoard extends Application {
+        private int[][] guiBoard;
+
+        private void fillGuiBoard() {
+
+            guiBoard = fillBoard();
+        }
+
+        @Override
+        public void start(Stage primaryStage){
+            fillGuiBoard();
+
+                }
+    }
+
+
+
+
+
+         */
+
+
+
--- src/main/java/it/polimi/ingsw/model/Game/Game.java
+++ src/main/java/it/polimi/ingsw/model/Game/Game.java
@@ -131,6 +131,8 @@ public class Game extends Observable implements Serializable {
         }
     }
 
+
+
     /**
      * manage the action of the players
      */
