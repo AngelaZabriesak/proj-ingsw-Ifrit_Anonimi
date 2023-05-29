77e0f5f636d2bb27b6f9590b6ea4535604a76ab6
Elena Tortorella <elena.tortorella@mail.polimi.it>
2023-05-29 23:14:43 +0200

    Update ChatScene.java

--- src/main/java/it/polimi/ingsw/View/Scene/ChatScene.java
+++ src/main/java/it/polimi/ingsw/View/Scene/ChatScene.java
@@ -15,6 +15,7 @@ import java.awt.*;
 
 public class ChatScene extends InputObservable implements GenericScene {
 
+
     private GridPane gridPane;
     private TextArea chat;
     private Button send;
@@ -23,13 +24,16 @@ public class ChatScene extends InputObservable implements GenericScene {
     private static Stage stageInstance;
 
     public void start(Stage primaryStage) {
+
+
         stageInstance = primaryStage;
         gridPane = new GridPane();
         chat = new TextArea();
         send = new Button("Chat");
+        String urlT = "file:src/main/resources/images/page_button.png";
         gridPane.add(chat,0,0);
 
-        stageInstance.setTitle("JavaFX Chat");
+        stageInstance.setTitle("MyShelfie Chat");
 
         BorderPane root = new BorderPane();
 
@@ -39,10 +43,17 @@ public class ChatScene extends InputObservable implements GenericScene {
         HBox inputBox = new HBox();
         inputBox.setPadding(new Insets(10));
         inputBox.setSpacing(10);
+        inputBox.setStyle("-fx-background-image: url('" + urlT + "'); " +
+                " -fx-background-repeat: no-repeat;" +
+                " -fx-background-size: stretch;");
         inputBox.setAlignment(Pos.CENTER);
+
 
         messageTextArea = new TextArea();
         messageTextArea.setPrefWidth(200);
+        messageTextArea.setStyle("-fx-background-image: url('" + urlT + "'); " +
+                "-fx-background-repeat: no-repeat;"  +
+                " -fx-background-size: stretch;");
         inputBox.getChildren().add(messageTextArea);
 
         send = new Button("Send");
@@ -50,6 +61,11 @@ public class ChatScene extends InputObservable implements GenericScene {
         inputBox.getChildren().add(send);
 
         root.setBottom(inputBox);
+
+        String urlB = "file:src/main/resources/images/parquet_background.jpg";
+        root.setStyle("-fx-background-image: url('" + urlB + "'); " +
+                "-fx-background-repeat: no-repeat;" +
+                "-fx-background-size: 400 300;");
 
         Scene scene = new Scene(root, 400, 300);
         stageInstance.setScene(scene);
